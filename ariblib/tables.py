2f6bb79a1d2a8089f984a0d73cb0876a6c92ddb7
youzaka <github@txqz.net>
2012-09-28 22:55:05 +0900

    setup.pyの名前付き引数が間違っていたのを修正

---
+++ ariblib/tables.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.2
+"""セクションとテーブルの違いがわからない頃の実装の後方互換"""
+from sections import *
+
+Table = Section
+ProgramAssociationTable = ProgramAssociationSection
+ProgramMapTable = ProgramMapSection
+NetworkInformationTable = NetworkInformationSection
+ServiceDescriptionTable = ServiceDescriptionSection
+BouquetAssociationTable = BouquetAssociationSection
+EventInformationTable = EventInformationSection
+RunningStatusTable = RunningStatusSection
+TimeAndDateTable = TimeAndDateSection
+TimeOffsetTable = TimeOffsetSection
+LocalEventInformationTable = LocalEventInformationSection
+EventRelationTable = EventRelationSection
+IndexTransmissionTable = IndexTransmissionSection
+PartialContentAnnouncementTable = PartialContentAnnouncementSection
+StuffingTable = StuffingSection
+BroadcasterInformationTable = BroadcasterInformationSection
+NetworkBoardInformationTable = NetworkBoardInformationSection
+CommonDataTable = CommonDataSection
+LinkedDescriptionTable = LinkedDescriptionSection
+SoftwareDownloadTriggerTable = SoftwareDownloadTriggerSection
+CommonDataTable = CommonDataSection
+DSMCCTable = DSMCCSection
+
