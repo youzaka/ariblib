de88a76995cf1f31430dfe87b612a89c0043eda9
youzaka <github@txqz.net>
2013-07-21 11:09:13 +0900

    最初のSDTのみパース対象にするオプションを追加

--- ariblib/service.py
+++ ariblib/service.py
@@ -7,7 +7,7 @@ from ariblib.sections import (ServiceDescriptionSection,
     ActualStreamServiceDescriptionSection,
     OtherStreamServiceDescriptionSection)
 
-def services(ts, channel_id=None, stream=None):
+def services(ts, channel_id=None, single=False, stream=None):
     """トランスポートストリームから Service オブジェクトを返すジェネレータ"""
 
     if channel_id is None:
@@ -22,9 +22,14 @@ def services(ts, channel_id=None, stream=None):
     else:
         SDT = ServiceDescriptionSection
 
-    for sdt in ts.sections(SDT):
+    if single:
+        sdt = next(ts.sections(SDT))
         for service in sdt.services:
             yield Service(service, get_channel_id(sdt))
+    else:
+        for sdt in ts.sections(SDT):
+            for service in sdt.services:
+                yield Service(service, get_channel_id(sdt))
 
 def parse_tsid(tsid):
     # NHK-BS対応
