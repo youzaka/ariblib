a63769769b68be57c0b5c9a912ad6f8448b2e16e
youzaka <youzaka@users.noreply.github.com>
2015-10-12 19:43:46 +0900

    PMTの記述子とテストを追加

--- ariblib/descriptors.py
+++ ariblib/descriptors.py
@@ -18,6 +18,17 @@ class descriptors(mnemonic):
             start = block_end
 
 
+tags = {}
+
+
+def tag(tag_id):
+    def wrapper(cls):
+        cls._tag = tag_id
+        tags[tag_id] = cls
+        return cls
+    return wrapper
+
+
 class Descriptor(Syntax):
 
     descriptor_tag = uimsbf(8)
@@ -26,4 +37,65 @@ class Descriptor(Syntax):
 
     @staticmethod
     def get(tag):
-        return Descriptor
+        return tags.get(tag, Descriptor)
+
+
+@tag(0x09)
+class ConditionalAccessDescriptor(Descriptor):
+
+    """限定受信方式記述子 (ARIB-TR-B14-4.3.2.2.1, ARIB-TR-B15-4.3.2.2.1)"""
+
+    descriptor_tag = uimsbf(8)
+    descriptor_length = uimsbf(8)
+    CA_system_ID = uimsbf(16)
+    reserved = bslbf(3)
+    CA_PID = uimsbf(13)
+    private_data_bytes = bslbf(descriptor_length - 4)
+
+
+@tag(0x52)
+class StreamIdentifierDescriptor(Descriptor):
+
+    """ストリーム識別記述子 (ARIB-STD-B10-2.6.2.16)"""
+
+    descriptor_tag = uimsbf(8)
+    descriptor_length = uimsbf(8)
+    component_tag = uimsbf(8)
+
+
+@tag(0xC1)
+class DigitalCopyControlDescriptor(Descriptor):
+
+    """デジタルコピー制御記述子 (ARIB-STD-B10-2.6.2.23)"""
+
+    descriptor_tag = uimsbf(8)
+    descriptor_length = uimsbf(8)
+    digital_recording_control_data = bslbf(2)
+    maximum_bitrate_flag = bslbf(1)
+    component_control_flag = bslbf(1)
+    copy_control_type = bslbf(2)
+    APS_control_data = bslbf(2)
+
+
+@tag(0xC8)
+class VideoDecodeControlDescriptor(Descriptor):
+
+    """ビデオでコードコントロール記述子 (ARIB-STD-B10-2.6.2.30)"""
+
+    descriptor_tag = uimsbf(8)
+    descriptor_length = uimsbf(8)
+    still_picture_flag = bslbf(1)
+    sequence_end_code_flag = bslbf(1)
+    video_encode_format = bslbf(4)
+    reserved_future_use = bslbf(2)
+
+
+@tag(0xFD)
+class DataComponentDescriptor(Descriptor):
+
+    """データ符号化記述子 (ARIB-STD-B10-2.6.2.10)"""
+
+    descriptor_tag = uimsbf(8)
+    descriptor_length = uimsbf(8)
+    data_component_id = uimsbf(16)
+    additional_data_component_info = uimsbf(descriptor_length - 2)
--- tests/test_sections.py
+++ tests/test_sections.py
@@ -81,3 +81,195 @@ class TestProgramMapSection(TestCase):
             self.assertEqual(pmt.section_number, 0)
             self.assertEqual(pmt.last_section_number, 0)
             self.assertEqual(pmt.PCR_PID, 0x100)
+
+            descriptors = pmt.descriptors
+            desc1 = next(descriptors)
+            self.assertEqual(desc1.descriptor_tag, 0x09)
+            self.assertEqual(desc1.CA_system_ID, 0x05)
+            self.assertEqual(desc1.CA_PID, 0x31)
+
+            desc2 = next(descriptors)
+            self.assertEqual(desc2.descriptor_tag, 0xC1)
+            self.assertEqual(desc2.digital_recording_control_data, 2)
+            self.assertFalse(desc2.maximum_bitrate_flag)
+            self.assertFalse(desc2.component_control_flag)
+            self.assertEqual(desc2.copy_control_type, 1)
+            self.assertEqual(desc2.APS_control_data, 0)
+            with self.assertRaises(StopIteration):
+                next(descriptors)
+
+            maps = pmt.maps
+            map1 = next(maps)
+            self.assertEqual(map1.stream_type, 0x02)
+            self.assertEqual(map1.elementary_PID, 0x121)
+            map1_descriptors = map1.descriptors
+            map1_desc1 = next(map1_descriptors)
+            self.assertEqual(map1_desc1.descriptor_tag, 0x52)
+            self.assertEqual(map1_desc1.component_tag, 0x0)
+            map1_desc2 = next(map1_descriptors)
+            self.assertEqual(map1_desc2.descriptor_tag, 0xC8)
+            self.assertFalse(map1_desc2.still_picture_flag)
+            self.assertTrue(map1_desc2.sequence_end_code_flag)
+            self.assertEqual(map1_desc2.video_encode_format, 4)
+            with self.assertRaises(StopIteration):
+                next(map1_descriptors)
+
+            map2 = next(maps)
+            self.assertEqual(map2.stream_type, 0x0F)
+            self.assertEqual(map2.elementary_PID, 0x112)
+            map2_descriptors = map2.descriptors
+            map2_desc1 = next(map2_descriptors)
+            self.assertEqual(map2_desc1.descriptor_tag, 0x52)
+            self.assertEqual(map2_desc1.component_tag, 0x10)
+            with self.assertRaises(StopIteration):
+                next(map2_descriptors)
+
+            map3 = next(maps)
+            self.assertEqual(map3.stream_type, 0x0D)
+            self.assertEqual(map3.elementary_PID, 0x740)
+            map3_descriptors = map3.descriptors
+            map3_desc1 = next(map3_descriptors)
+            self.assertEqual(map3_desc1.descriptor_tag, 0x52)
+            self.assertEqual(map3_desc1.component_tag, 0x40)
+            map3_desc2 = next(map3_descriptors)
+            self.assertEqual(map3_desc2.descriptor_tag, 0xFD)
+            self.assertEqual(map3_desc2.data_component_id, 0x0C)
+            self.assertEqual(map3_desc2.additional_data_component_info,
+                             0x333F00030003FFBF)
+            with self.assertRaises(StopIteration):
+                next(map3_descriptors)
+
+            map4 = next(maps)
+            self.assertEqual(map4.stream_type, 0x0D)
+            self.assertEqual(map4.elementary_PID, 0x750)
+            map4_descriptors = map4.descriptors
+            map4_desc1 = next(map4_descriptors)
+            self.assertEqual(map4_desc1.descriptor_tag, 0x52)
+            self.assertEqual(map4_desc1.component_tag, 0x50)
+            map4_desc2 = next(map4_descriptors)
+            self.assertEqual(map4_desc2.descriptor_tag, 0xFD)
+            self.assertEqual(map4_desc2.data_component_id, 0x0C)
+            self.assertEqual(map4_desc2.additional_data_component_info,
+                             0x1FFFBF)
+            with self.assertRaises(StopIteration):
+                next(map4_descriptors)
+
+            map5 = next(maps)
+            self.assertEqual(map5.stream_type, 0x0D)
+            self.assertEqual(map5.elementary_PID, 0x751)
+            map5_descriptors = map5.descriptors
+            map5_desc1 = next(map5_descriptors)
+            self.assertEqual(map5_desc1.descriptor_tag, 0x52)
+            self.assertEqual(map5_desc1.component_tag, 0x51)
+            map5_desc2 = next(map5_descriptors)
+            self.assertEqual(map5_desc2.descriptor_tag, 0xFD)
+            self.assertEqual(map5_desc2.data_component_id, 0x0C)
+            self.assertEqual(map5_desc2.additional_data_component_info,
+                             0x1FFFBF)
+            with self.assertRaises(StopIteration):
+                next(map5_descriptors)
+
+            map6 = next(maps)
+            self.assertEqual(map6.stream_type, 0x0D)
+            self.assertEqual(map6.elementary_PID, 0x752)
+            map6_descriptors = map6.descriptors
+            map6_desc1 = next(map6_descriptors)
+            self.assertEqual(map6_desc1.descriptor_tag, 0x52)
+            self.assertEqual(map6_desc1.component_tag, 0x52)
+            map6_desc2 = next(map6_descriptors)
+            self.assertEqual(map6_desc2.descriptor_tag, 0xFD)
+            self.assertEqual(map6_desc2.data_component_id, 0x0C)
+            self.assertEqual(map6_desc2.additional_data_component_info,
+                             0x1FFFBF)
+            with self.assertRaises(StopIteration):
+                next(map6_descriptors)
+
+            map7 = next(maps)
+            self.assertEqual(map7.stream_type, 0x0D)
+            self.assertEqual(map7.elementary_PID, 0x960)
+            map7_descriptors = map7.descriptors
+            map7_desc1 = next(map7_descriptors)
+            self.assertEqual(map7_desc1.descriptor_tag, 0x52)
+            self.assertEqual(map7_desc1.component_tag, 0x60)
+            map7_desc2 = next(map7_descriptors)
+            self.assertEqual(map7_desc2.descriptor_tag, 0xFD)
+            self.assertEqual(map7_desc2.data_component_id, 0x0C)
+            self.assertEqual(map7_desc2.additional_data_component_info,
+                             0x1FFFBF)
+            with self.assertRaises(StopIteration):
+                next(map7_descriptors)
+
+            map8 = next(maps)
+            self.assertEqual(map8.stream_type, 0x0D)
+            self.assertEqual(map8.elementary_PID, 0x961)
+            map8_descriptors = map8.descriptors
+            map8_desc1 = next(map8_descriptors)
+            self.assertEqual(map8_desc1.descriptor_tag, 0x52)
+            self.assertEqual(map8_desc1.component_tag, 0x61)
+            map8_desc2 = next(map8_descriptors)
+            self.assertEqual(map8_desc2.descriptor_tag, 0xFD)
+            self.assertEqual(map8_desc2.data_component_id, 0x0C)
+            self.assertEqual(map8_desc2.additional_data_component_info,
+                             0x1FFFBF)
+            with self.assertRaises(StopIteration):
+                next(map8_descriptors)
+
+            map9 = next(maps)
+            self.assertEqual(map9.stream_type, 0x0D)
+            self.assertEqual(map9.elementary_PID, 0x75E)
+            map9_descriptors = map9.descriptors
+            map9_desc1 = next(map9_descriptors)
+            self.assertEqual(map9_desc1.descriptor_tag, 0x52)
+            self.assertEqual(map9_desc1.component_tag, 0x5E)
+            map9_desc2 = next(map9_descriptors)
+            self.assertEqual(map9_desc2.descriptor_tag, 0x09)
+            self.assertEqual(map9_desc2.CA_system_ID, 0x05)
+            self.assertEqual(map9_desc2.CA_PID, 0x1FFF)
+            map9_desc3 = next(map9_descriptors)
+            self.assertEqual(map9_desc3.descriptor_tag, 0xFD)
+            self.assertEqual(map9_desc3.data_component_id, 0x0C)
+            self.assertEqual(map9_desc3.additional_data_component_info,
+                             0x1FFFBF)
+            with self.assertRaises(StopIteration):
+                next(map9_descriptors)
+
+            map10 = next(maps)
+            self.assertEqual(map10.stream_type, 0x0D)
+            self.assertEqual(map10.elementary_PID, 0x75F)
+            map10_descriptors = map10.descriptors
+            map10_desc1 = next(map10_descriptors)
+            self.assertEqual(map10_desc1.descriptor_tag, 0x52)
+            self.assertEqual(map10_desc1.component_tag, 0x5F)
+            map10_desc2 = next(map10_descriptors)
+            self.assertEqual(map10_desc2.descriptor_tag, 0x09)
+            self.assertEqual(map10_desc2.CA_system_ID, 0x05)
+            self.assertEqual(map10_desc2.CA_PID, 0x1FFF)
+            map10_desc3 = next(map10_descriptors)
+            self.assertEqual(map10_desc3.descriptor_tag, 0xFD)
+            self.assertEqual(map10_desc3.data_component_id, 0x0C)
+            self.assertEqual(map10_desc3.additional_data_component_info,
+                             0x1FFFBF)
+            with self.assertRaises(StopIteration):
+                next(map10_descriptors)
+
+            map11 = next(maps)
+            self.assertEqual(map11.stream_type, 0x0D)
+            self.assertEqual(map11.elementary_PID, 0x96E)
+            map11_descriptors = map11.descriptors
+            map11_desc1 = next(map11_descriptors)
+            self.assertEqual(map11_desc1.descriptor_tag, 0x52)
+            self.assertEqual(map11_desc1.component_tag, 0x6E)
+            map11_desc2 = next(map11_descriptors)
+            self.assertEqual(map11_desc2.descriptor_tag, 0x09)
+            self.assertEqual(map11_desc2.CA_system_ID, 0x05)
+            self.assertEqual(map11_desc2.CA_PID, 0x1FFF)
+            map11_desc3 = next(map11_descriptors)
+            self.assertEqual(map11_desc3.descriptor_tag, 0xFD)
+            self.assertEqual(map11_desc3.data_component_id, 0x0C)
+            self.assertEqual(map11_desc3.additional_data_component_info,
+                             0x1FFFBF)
+            with self.assertRaises(StopIteration):
+                next(map11_descriptors)
+
+            with self.assertRaises(StopIteration):
+                next(maps)
